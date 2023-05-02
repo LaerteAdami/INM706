9d55ae7a3cd274f7a2cbe0e38cb3aefaf300e694
Elisa Troschka <adcy353@gpu04.pri.hyperion.alces.network>
2023-05-02 11:31:51 +0100

    multi-layer lstm

--- Utilities/lstmHandler.py
+++ Utilities/lstmHandler.py
@@ -25,6 +25,8 @@ class DecoderLSTM(nn.Module):
         super(DecoderLSTM, self).__init__()
         
         self.D = 2 if bidirectional else 1
+        self.n_layers = num_layers
+
         
         self.emb = nn.Embedding(vocabulary_size, embedding_size)
         self.lstm = nn.LSTM(input_size = embedding_size, hidden_size = embedding_size, 
@@ -36,10 +38,6 @@ class DecoderLSTM(nn.Module):
         
         x = self.emb(x)
         x = self.relu(x)
-        #print("x: {}".format(x.size()))
-        #print("h: {}".format(hidden[0].size()))
-        #print("c: {}".format(hidden[1].size()))
-              
         x, hidden = self.lstm(x, h) 
         output = self.lin(x) 
         
@@ -56,11 +54,12 @@ class AttentionDecoderLSTM(nn.Module):
         self.seq_len = seq_len
         self.emb_size = embedding_size
         self.D = 2 if bidirectional else 1
+        self.n_layers = num_layers
         
         self.emb = nn.Embedding(vocabulary_size, embedding_size)
-        self.alignment = nn.Linear(embedding_size * (self.D + 1), seq_len)
-        self.norm_align = nn.Softmax(dim=1)
-        self.lstm = nn.LSTM(input_size = embedding_size, hidden_size = embedding_size, 
+        self.alignment = nn.Linear(embedding_size * (self.D * self.n_layers + 1), seq_len)
+        self.softmax = nn.Softmax(dim=1)
+        self.lstm = nn.LSTM(input_size = embedding_size * (self.D + 1), hidden_size = embedding_size, 
                             num_layers = num_layers, batch_first = True, bidirectional = bidirectional)
         self.relu = nn.ReLU(inplace=False)
         self.lin = nn.Linear(self.D * embedding_size, vocabulary_size)
@@ -71,23 +70,21 @@ class AttentionDecoderLSTM(nn.Module):
         # x: (batch, 1, emb), y: (batch, seq, D * emb), h: (D, batch, emb), c: (1, batch, emb)
         
         x = self.emb(x)
-        #print(f'x: {x.size()}, y: {y.size()}, h: {h.size()}, c: {c.size()}')
+        # print(f'x: {x.size()}, y: {y.size()}, h: {h.size()}, c: {c.size()}')
         
         # alignment: (emb * 2) -> (seq)
-        attention_weights = self.norm_align(
+        attention_weights = self.softmax(
             self.alignment(
-                torch.cat((x, h.view(-1, 1, self.D * self.emb_size)), dim=-1)
+                torch.cat((x, h.view(-1, 1, self.n_layers * self.D * self.emb_size)), dim=-1)
             )
         )
         # context_vector: lc(attention_weight, y) size: (1, batch, emb)
         context_vector = torch.bmm(attention_weights, y)
-        
-        context_vector = self.relu(context_vector).view(self.D, -1, self.emb_size)
-        output, hidden = self.lstm(x, (context_vector, c)) # x: (emb_size)
+        context_vector = self.relu(context_vector)
+        input_decoder = torch.cat((x, context_vector), dim=-1)
+        output, hidden = self.lstm(input_decoder, (h, c)) # x: (emb_size)
         output = self.lin(output) 
         
-        return output, hidden       
-        
-        
+        return output, hidden        
         
         
